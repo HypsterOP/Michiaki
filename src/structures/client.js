8d81aa9e2c636189597b01cb348ae2dde7692d67
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-08-27 09:01:40 +0530

    fix: (database) use pool and log events

--- src/structures/client.js
+++ src/structures/client.js
@@ -46,9 +46,9 @@ module.exports = class client extends Client {
 
     this.config = require("../config");
 
-    const { Client } = require('pg');
+    const { Client, Pool } = require('pg');
 
-    const database = new Client({
+    const database = new Pool({
       user: "postgres",
       password: process.env.PASSWORD,
       port: 5432,
@@ -56,8 +56,14 @@ module.exports = class client extends Client {
       database: "michiaki"
     });
 
-    database.connect().then(() => {
-      log.success(`DATABASE`, `Connected to postgres`)
+    database.on("error", (err) => {
+      log.error("Postgres error", err)
+    });
+
+    database.connect()
+
+    database.on("connect", () => {
+      log.success("Database", "Connected to postgres")
     })
 
     this.distube = new DisTube.DisTube(this, {
