fb1e5080bb032968287dd27128911050343aba41
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-08-27 09:41:08 +0530

    fix: (folders) use folders for database

---
+++ src/database/clients/postgres.js
@@ -0,0 +1,21 @@
+const { Client, Pool } = require("pg")
+
+const database = new Pool({
+  user: "postgres",
+  password: process.env.PASSWORD,
+  port: 5432,
+  host: "localhost",
+  database: "michiaki",
+})
+
+database.on("error", (err) => {
+  log.error("Postgres error", err)
+})
+
+database.connect()
+
+database.on("connect", () => {
+  log.success("Database", "Connected to postgres")
+})
+
+module.exports = database;
--- src/structures/client.js
+++ src/structures/client.js
@@ -46,26 +46,6 @@ module.exports = class client extends Client {
 
     this.config = require("../config");
 
-    const { Client, Pool } = require('pg');
-
-    const database = new Pool({
-      user: "postgres",
-      password: process.env.PASSWORD,
-      port: 5432,
-      host: "localhost",
-      database: "michiaki"
-    });
-
-    database.on("error", (err) => {
-      log.error("Postgres error", err)
-    });
-
-    database.connect()
-
-    database.on("connect", () => {
-      log.success("Database", "Connected to postgres")
-    })
-
     this.distube = new DisTube.DisTube(this, {
       searchSongs: 4,
       emitNewSongOnly: false,
